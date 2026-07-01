017c0710c1e0a077902a689d85255c545e73206b
Arnaud Rocher <arnaud.roche3@gmail.com>
2026-07-01 16:32:22 +0200

    chore(test): use real model compiler for integration test

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,20 +1,4 @@
-use std::fs;
-use std::path::Path;
-
-use escargot::CargoBuild;
-
 #[test]
 fn cli() {
-    let model_compiler_dir = Path::new(env!("CARGO_TARGET_TMPDIR"));
-    let model_compiler_path = model_compiler_dir.join("Opc.Ua.ModelCompiler");
-    let fake_model_compiler = CargoBuild::new()
-        .package("fake-model-compiler")
-        .run()
-        .expect("building fake model compiler should not fail");
-    fs::copy(fake_model_compiler.path(), model_compiler_path)
-        .expect("copying built fake compiler should not fail");
-
-    trycmd::TestCases::new()
-        .env("PATH", model_compiler_dir.to_string_lossy())
-        .case("tests/cmd/cli.toml");
+    trycmd::TestCases::new().case("tests/cmd/cli.toml");
 }
